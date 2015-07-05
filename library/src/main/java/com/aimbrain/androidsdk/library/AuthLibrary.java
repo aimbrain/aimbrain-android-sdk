87d6bfa2cabbc1cd699a2bdb9b492c4746c8ea93
AimBrain <founders@aimbrain.com>
2015-07-05 16:12:42 +0100

    added local actionToString method to support Android versions 14 and up

--- library/src/main/java/com/aimbrain/androidsdk/library/AuthLibrary.java
+++ library/src/main/java/com/aimbrain/androidsdk/library/AuthLibrary.java
@@ -46,7 +46,7 @@ public class AuthLibrary {
                     for (int p = 0; p < pointerCount; p++) {
                         try {
                             JSONObject eventObj = new JSONObject()
-                                    .put("a", MotionEvent.actionToString(event.getAction()))
+                                    .put("a", actionToString(event.getAction()))
                                     .put("t", event.getHistoricalEventTime(h))
                                     .put("i", event.getPointerId(p))
                                     .put("x", event.getHistoricalX(p, h))
@@ -69,7 +69,7 @@ public class AuthLibrary {
                 for (int p = 0; p < pointerCount; p++) {
                     try {
                         JSONObject eventObj = new JSONObject()
-                                .put("a", MotionEvent.actionToString(event.getAction()))
+                                .put("a", actionToString(event.getAction()))
                                 .put("t", event.getEventTime())
                                 .put("i", event.getPointerId(p))
                                 .put("x", event.getX(p))
@@ -102,6 +102,7 @@ public class AuthLibrary {
             e.printStackTrace();
         }
 
+        //Log.d("REQUEST BODY:", body.toString());
         new CallAPI().execute(new CallAPIRequest("auth", body.toString(), handler));
     }
 
@@ -303,4 +304,36 @@ public class AuthLibrary {
         }
     } // end CallAPIResponse
 
+
+    private static String actionToString(int action) {
+        switch (action) {
+            case MotionEvent.ACTION_DOWN:
+                return "ACTION_DOWN";
+            case MotionEvent.ACTION_UP:
+                return "ACTION_UP";
+            case MotionEvent.ACTION_CANCEL:
+                return "ACTION_CANCEL";
+            case MotionEvent.ACTION_OUTSIDE:
+                return "ACTION_OUTSIDE";
+            case MotionEvent.ACTION_MOVE:
+                return "ACTION_MOVE";
+            case MotionEvent.ACTION_HOVER_MOVE:
+                return "ACTION_HOVER_MOVE";
+            case MotionEvent.ACTION_SCROLL:
+                return "ACTION_SCROLL";
+            case MotionEvent.ACTION_HOVER_ENTER:
+                return "ACTION_HOVER_ENTER";
+            case MotionEvent.ACTION_HOVER_EXIT:
+                return "ACTION_HOVER_EXIT";
+        }
+        int index = (action & MotionEvent.ACTION_POINTER_INDEX_MASK) >> MotionEvent.ACTION_POINTER_INDEX_SHIFT;
+        switch (action & MotionEvent.ACTION_MASK) {
+            case MotionEvent.ACTION_POINTER_DOWN:
+                return "ACTION_POINTER_DOWN(" + index + ")";
+            case MotionEvent.ACTION_POINTER_UP:
+                return "ACTION_POINTER_UP(" + index + ")";
+            default:
+                return Integer.toString(action);
+        }
+    }
 }
